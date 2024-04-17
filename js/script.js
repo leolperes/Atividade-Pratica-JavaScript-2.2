e957b5215ceb8beeef5aa81b4147284d81266e85
Leolperes <leo.lperes@gmail.com>
2024-04-16 22:00:53 -0300

    começa parte 2

--- js/script.js
+++ js/script.js
@@ -15,3 +15,14 @@ function mostrarApenasConta (homeId, loginId, cadastroId) {
     document.getElementById(loginId).classList.replace("show", "oculto");
     document.getElementById(cadastroId).classList.replace("oculto","show");
 }
+
+function checarFormValido () {
+    const campo1 = document.getElementById("login-email");
+    const campo2 = document.getElementById("login-password");
+
+    if (campo1 !== "" & campo2 !== "") {
+        document.getElementById("botaoLogin").classList.replace("oculto", "show-flex");
+    } else {
+        document.getElementById("botaoLogin").classList.replace("show-flex", "oculto");
+    }
+}
